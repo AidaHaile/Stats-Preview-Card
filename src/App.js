9c846e98e0459e8814a8b46c4d6cb1b511f39213
unknown <aidathaile@gmail.com>
2021-07-30 21:25:43 +1000

    Mobile responsive edits

--- src/App.js
+++ src/App.js
@@ -5,25 +5,23 @@ import Picture from './images/image-header-desktop.jpg'
 function App() {
   return (
     <MainContainer className="Bodyclass">
-    <ContainerOne>
-    <MainBox>
-      <Title className="Title">Get <span>insights</span> that help <br/> your business grow.</Title>
-        <p>
-          Discover the benefits of data analytics and make <br/> better decisions regarding revenue, customer <br/>
-          experience, and overall efficiency.
-        </p>
-        <Box>
-        <Number>10k+</Number>
-        <Number>314</Number>
-        <Number>12M+</Number>
-        <Text>companies</Text>
-        <Text>templates</Text>
-        <Text>queries</Text>
-        </Box>
-      </MainBox>
-    </ContainerOne>
-    <ContainerTwo>
-    </ContainerTwo>
+      <LeftBox>
+        <Title>Get <span>insights</span> that help your business grow.</Title>
+        <Section>
+          Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+        </Section>
+        <NumSection>
+        <NumberOne>10k+</NumberOne>
+        <NumberTwo>314</NumberTwo>
+        <NumberThree>12M+</NumberThree>
+        </NumSection>
+        <TextSection>
+        <TextOne>Companies</TextOne>
+        <TextTwo>Templates</TextTwo>
+        <TextThree>Queries</TextThree>
+        </TextSection>
+      </LeftBox>
+      <RightBox></RightBox>
     </MainContainer>
   );
 }
@@ -31,77 +29,190 @@ function App() {
 
 
 const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  wrap: nowrap;
-  min-height: 100vh; // this is the height of screen -> set so don't get a zero height and object can move
-`;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas:
+  "left right";
+  padding-left: 19vw;
+  padding-right: 19vw;
+  padding-top: 14vw;
+  padding-bottom: 18vw;
+  text-align: left;
 
 
-const MainBox = styled.div`
-  margin-top: 80px;
-  margin-left: 70px;
-`;
-
-const ContainerOne = styled.div`
-  background-color: hsl(244, 38%, 16%);
-  height: 450px;
-  width: 550px;
-  border-radius: 5px 0px 0px 5px;
-  p{
-    color: hsla(0, 0%, 100%, 0.75);
-    margin-top: 25px;
-    line-height: 25px;
-    font-size: 15px;
+  @media only screen and (max-width: 550px){
+    grid-template-columns: 1fr;
+    grid-template-rows: 240px 1fr;
+    grid-template-areas:
+    "right"
+    "left";
+    text-align: center;
+    padding: 0;
   }
 `;
+
+
+const LeftBox = styled.div`
+  background-color: hsl(244, 38%, 16%);
+  max-height: 550px;
+  border-radius: 5px 0px 0px 5px;
+  grid-area: left;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 0.25fr 0.2fr;
+  padding-top: 5rem;
+  padding-left: 5rem;
+  padding-bottom: 3rem;
+  padding-right: 9rem;
+  grid-template-areas: 
+  "title"
+  "section"
+  "numsection"
+  "textsection";
+
+
+  @media only screen and (max-width: 550px){
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr repeat(6,35px);
+    grid-template-areas:
+    "title"
+    "section"
+    "num1"
+    "text1"
+    "num2"
+    "text2"
+    "num3"
+    "text3";
+    padding: 20px 20px 50px 20px;
+    border-radius: 0px 0px 5px 5px;
+  }
+
+`;
+
 
 const Title = styled.div`
+  grid-area: title;
   font-size: 35px;
+  font-weight: bold;
+  color: white;
+
+  span{
+    color: hsl(277, 64%, 61%);
+  }
+
+  @media only screen and (max-width: 550px){
+    font-size: 29px;
+    padding-bottom: 15px;
+  }
+
+`;
+
+const Section = styled.div`
+  grid-area: section;
+  line-height: 25px;
+  //background-color: purple;
+  color: hsla(0, 0%, 100%, 0.75);
+  font-size: 15px;
+  padding-bottom: 50px;
+`;
+
+
+const NumSection = styled.div`
+  grid-area: numsection;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas:
+  "num1 num2 num3";
+  font-size: 23px;
   color: white;
   font-weight: bold;
-  span {
-  color: hsl(277, 64%, 61%);
-  display: inline;
+
+  @media only screen and (max-width: 550px){
+    display: contents
+  }
+
+`;
+
+const NumberOne = styled.div`
+  grid-area: num1;
+  //background-color: orange;
+
+`;
+
+
+const NumberTwo = styled.div`
+  grid-area: num2;
+  //background-color: pink;
+`;
+const NumberThree = styled.div`
+  grid-area: num3;
+  //background-color: yellow;
+`;
+
+const TextSection = styled.div`
+  grid-area: textsection;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas:
+  "text1 text2 text3";
+  font-size: 15px;
+  color: hsla(0, 0%, 100%, 0.6);
+
+
+  @media only screen and (max-width: 550px){
+    display: contents;
+  }
+
+`;
+
+const TextOne = styled.div`
+  grid-area: text1;
+  //background-color: brown;
+  
+  @media only screen and (max-width: 550px){
+    padding-bottom: 20px;
+  }
+
+`;
+
+const TextTwo = styled.div`
+  grid-area: text2;
+  //background-color: green;
+
+  @media only screen and (max-width: 550px){
+    padding-bottom: 20px;
   }
 `;
 
-const ContainerTwo = styled.div`
+const TextThree = styled.div`
+  grid-area: text3;
+  //background-color: grey;
+
+  @media only screen and (max-width: 550px){
+    padding-bottom: 20px;
+  }
+`;
+
+const RightBox = styled.div`
+  border-radius: 0px 5px 5px 0px;
+  max-height: 550px;
+  grid-area: right;
+  //background-color: brown;
   background-image:  linear-gradient(  
     rgba(83, 14, 126, 0.57), 
     rgba(83, 14, 126, 0.57)
   ), url(${Picture});
-  height: 450px;
-  width: 550px;
   background-repeat: no-repeat;
   background-size: 100% 100%; // fits the entire image
-  border-radius: 0px 5px 5px 0px;
-`;
 
-
-const Box = styled.div`
-  display: grid;
-  width: 90%;
-  grid-template-columns: auto auto auto;
-  margin-top: 60px;
-
-`;
-
-
-const Number = styled.div`
-  font-size: 23px;
-  font-weight: bold;
-  color: white;
-`;
-
-
-const Text = styled.div`
-  text-transform: uppercase;
-  font-size: 13px;
-  color: hsla(0, 0%, 100%, 0.6);
-  margin-top: 5px;
-  
+  @media only screen and (max-width: 550px){
+    background-color: orange;
+    border-radius: 5px 5px 0px 0px;
+  }
 `;
 
 export default App;
